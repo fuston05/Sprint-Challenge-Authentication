9bd347789ae13e5b07fa9b5a5d0ed1ff740afb6b
fuston05 <fuston05@yahoo.com>
2020-03-27 12:22:04 -0400

    got endpoints working, generated a token

---
+++ auth/users_model.js
@@ -0,0 +1,14 @@
+const db= require('../database/dbConfig');
+
+module.exports= {
+  add,
+  findBy,
+}
+
+function add(newUser){
+  return db('users').insert(newUser);
+}//end add
+
+function findBy(filter){
+  return db('users').where(filter);
+}//end find
