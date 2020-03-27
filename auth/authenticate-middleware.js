c81b19710e671e90177618c1d530221be0799f12
fuston05 <fuston05@yahoo.com>
2020-03-27 12:32:41 -0400

    got auth middleware done, access granted to jokes at this point yaaay dadJokes

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,23 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt= require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const {authorization}= req.headers;
+  const secret= 'keep it secret!';
+
+  if(authorization){
+    jwt.verify(authorization, secret, (error, decodedToken) => {
+      if(error){
+        res.status(401).json({ message: "Invalid credentials" })
+      }else{
+        req.decodedToken= decodedToken;
+        next();
+      }//end if/else error
+    });
+  }else{
+    res.status(401).json({ you: 'shall not pass!' });
+  }
+
 };
